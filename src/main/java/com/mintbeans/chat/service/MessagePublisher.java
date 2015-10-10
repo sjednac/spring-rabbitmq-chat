73e74889e36ef7908704ff6ab5745a35fa3919a8
Szymon Biliński <szymon.bilinski@gmail.com>
2015-10-10 18:27:32 +0200

    Switched RabbitTemplate to AmqpTemplate.

--- src/main/java/com/mintbeans/chat/service/MessagePublisher.java
+++ src/main/java/com/mintbeans/chat/service/MessagePublisher.java
@@ -3,7 +3,7 @@ package com.mintbeans.chat.service;
 import com.mintbeans.chat.AppSettings;
 import com.mintbeans.chat.model.Message;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.core.AmqpTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -11,19 +11,19 @@ import org.springframework.stereotype.Component;
 @Component
 public class MessagePublisher {
 
-	private final RabbitTemplate rabbitTemplate;
+	private final AmqpTemplate amqpTemplate;
 
 	private final String exchange;
 
 	@Autowired
-	public MessagePublisher(AppSettings settings, RabbitTemplate rabbitTemplate) {
-		this.rabbitTemplate = rabbitTemplate;
+	public MessagePublisher(AppSettings settings, AmqpTemplate amqpTemplate) {
+		this.amqpTemplate = amqpTemplate;
 		this.exchange = settings.getRabbit().getExchange();
 	}
 
 	public void publish(Message msg) {
 		log.debug("Publishing message: {}", msg);
-		rabbitTemplate.convertAndSend(exchange, null, msg);
+		amqpTemplate.convertAndSend(exchange, null, msg);
 	}
 
 }
