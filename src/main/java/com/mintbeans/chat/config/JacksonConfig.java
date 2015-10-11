f549ee484c358c3368f93b3903db41a2acac09df
Szymon Biliński <szymon.bilinski@gmail.com>
2015-10-11 16:08:16 +0200

    Removed "web" dependencies.

--- src/main/java/com/mintbeans/chat/config/JacksonConfig.java
+++ src/main/java/com/mintbeans/chat/config/JacksonConfig.java
@@ -1,12 +1,13 @@
 package com.mintbeans.chat.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import com.mintbeans.chat.AppSettings;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 
 @Slf4j
 @Configuration
@@ -14,10 +15,11 @@ public class JacksonConfig {
 
 	@Bean
 	@Autowired
-	public Jackson2ObjectMapperBuilder objectMapperBuilder(AppSettings settings) {
-		final Jackson2ObjectMapperBuilder builder = new Jackson2ObjectMapperBuilder();
-		return builder.indentOutput(settings.isIndentJson())
-					  .modules(new JavaTimeModule());
+	public ObjectMapper objectMapper(AppSettings settings) {
+		final ObjectMapper objectMapper = new ObjectMapper();
+		objectMapper.registerModule(new JavaTimeModule());
+		objectMapper.enable(SerializationFeature.INDENT_OUTPUT);
+		return objectMapper;
 	}
 
 }
